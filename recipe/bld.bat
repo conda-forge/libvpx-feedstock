a396c38734eaead8b2e1ebddb5bc2cfa8851d3f0
Mark Harfouche <mark.harfouche@gmail.com>
2022-07-07 00:00:18 -0400

    simplify

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,2 +1,5 @@
-call %BUILD_PREFIX%\Library\bin\run_autotools_clang_conda_build.bat
+bash configure --prefix=%LIBRARY_PREFIX%\ ^
+  --target=x86_64-win64-vs14 ^
+  --as=yasm
+
 if %ERRORLEVEL% neq 0 exit 1
