71ee8613ad8003ffb5a02347187753b2273523af
Mark Harfouche <mark.harfouche@gmail.com>
2022-07-08 07:51:05 -0400

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -7,7 +7,7 @@ if %ERRORLEVEL% neq 0 exit 1
 bash -c "mkdir /tmp"
 
 bash configure --prefix=%LIBRARY_PREFIX%\ ^
-  --target=x86_64-win64-vs17 ^
+  --target=x86_64-win64-vs14 ^
   --as=yasm ^
   --libdir=%LIBRARY_PREFIX%\lib
 cat config.log
