54b5710ccfb863c90b4d077f5eed976011a1f3b5
Mark Harfouche <mark.harfouche@gmail.com>
2022-07-07 22:12:17 -0400

    List

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,8 @@
+dir %LIBRARY_PREFIX%
+if %ERRORLEVEL% neq 0 exit 1
+dir %LIBRARY_PREFIX%\lib
+if %ERRORLEVEL% neq 0 exit 1
+
 bash configure --prefix=%LIBRARY_PREFIX%\ ^
   --target=x86_64-win64-vs14 ^
   --as=yasm ^
